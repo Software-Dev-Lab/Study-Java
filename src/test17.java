1ccb1df52b3365d052b85e22ae449f4afa348af2
ZRMYDYCG <547471919@qq.com>
2024-10-30 20:30:37 +0800

    feat: 随机数

--- src/test17.java
+++ src/test17.java
@@ -1,5 +1,9 @@
+import java.util.Random;
+
 public class test17 {
     public static void main(String[] args) {
-
+        Random r = new Random ();
+        int number = r.nextInt(10);
+        System.out.println(number);
     }
 }
