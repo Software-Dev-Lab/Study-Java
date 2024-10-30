e92de133b0ca09fbc1fb4ec5b562480626595a86
ZRMYDYCG <547471919@qq.com>
2024-10-30 20:28:45 +0800

    feat: 吃包子 continue

--- src/test16.java
+++ src/test16.java
@@ -1,5 +1,13 @@
 public class test16 {
     public static void main(String[] args) {
-
+        // 1. 吃1~5号包子
+        for (int i = 1; i <= 5; i++) {
+            // 2. 第3个包子有虫子就跳过，继续吃下面的包子
+            if(i == 3){
+                // 跳过本次循环（本次循环中，下面的代码就不执行了），继续执行下次循环。
+                continue;
+            }
+            System.out.println("在吃第" + i + "个包子");
+        }
     }
 }
