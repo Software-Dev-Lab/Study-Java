a482e96bf763075023d2ed77ef845ee9da82359e
ZRMYDYCG <547471919@qq.com>
2024-10-30 19:00:58 +0800

    feat: 自增自减运算符

---
+++ src/test1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class test1 {
+    public static void main(String[] args) {
+        int a = 10;
+        a++;//就是让变量a里面的值 + 1
+        System.out.println(a);//11
+        ++a;//就是让变量a里面的值 + 1
+        System.out.println(a);//12
+    }
+}
