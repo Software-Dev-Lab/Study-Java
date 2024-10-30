6c44ec673e4c4f1a0affeefc1c570fd756066a58
ZRMYDYCG <547471919@qq.com>
2024-10-30 19:56:23 +0800

    feat: 影院选座

--- src/test11.java
+++ src/test11.java
@@ -1,3 +1,25 @@
+import java.util.Scanner;
+
 public class test11 {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        //分析：
+//1.键盘录入票号
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入票号");
+        int ticket = sc.nextInt();
+        if(ticket >= 1 && ticket <= 100){
+            //合法
+            //2.对票号进行判断
+            if (ticket % 2 == 0) {
+                //偶数
+                System.out.println("坐右边");
+            } else {
+                //奇数
+                System.out.println("坐左边");
+            }
+        }else{
+            //票号不合法
+            System.out.println("票号不合法");
+        }
+    }
 }
