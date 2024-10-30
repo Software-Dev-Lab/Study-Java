ab7c3c1aa05267345daffea7cb80c9071eb5b645
ZRMYDYCG <547471919@qq.com>
2024-10-30 20:05:48 +0800

    feat: swich case  jdk12

---
+++ src/test14.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class test14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入星期");
+        int week = sc.nextInt();//3
+        switch (week) {
+            case 1, 2, 3, 4, 5 -> System.out.println("工作日");
+            case 6, 7 -> System.out.println("休息日");
+            default -> System.out.println("没有这个星期");
+        }
+    }
+}
