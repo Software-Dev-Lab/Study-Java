7db73277846f7a9e5b0629a5a33ff8c40b58b7dd
ZRMYDYCG <547471919@qq.com>
2024-10-30 20:35:17 +0800

    feat: containue

---
+++ src/test18.java
@@ -0,0 +1,14 @@
+/**
+ * 逢 7 过
+ * */
+public class test18 {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 100; i++) {
+            if(i % 10 == 7 || i / 10  % 10 == 7 || i % 7 == 0) {
+                System.out.println("过");
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
