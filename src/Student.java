8d0cf386e27047fd5698ac4fada17f11a25e0d06
ZRMYDYCG <547471919@qq.com>
2024-10-31 18:20:56 +0800

    feat: 面向对象

---
+++ src/Student.java
@@ -0,0 +1,10 @@
+public class Student {
+    String name;
+    int age;
+    public void study() {
+        System.out.println("好好学习，天天向上");
+    }
+    public void doHomework() {
+        System.out.println("键盘敲烂，月薪过万");
+    }
+}
--- src/test22.java
+++ src/test22.java
@@ -1,2 +1,18 @@
 public class test22 {
+
+    public static void main(String[] args) {
+        //创建对象
+        Student s = new Student();
+
+        //使用对象
+        System.out.println(s.name + "," + s.age);
+
+        s.name = "林青霞";
+        s.age = 30;
+
+        System.out.println(s.name + "," + s.age);
+
+        s.study();
+        s.doHomework();
+    }
 }
