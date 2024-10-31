b4468b3354fe61048cb7c95710d27cb811b33842
ZRMYDYCG <547471919@qq.com>
2024-10-31 18:29:15 +0800

    feat: 构造方法的注意事项

--- src/Student.java
+++ src/Student.java
@@ -1,29 +1,20 @@
 public class Student {
-    //成员变量
     private String name;
     private int age;
 
-    public void study() {
-        System.out.println("好好学习，天天向上");
-    }
-    public void doHomework() {
-        System.out.println("键盘敲烂，月薪过万");
-    }
-    //get/set方法
-    public void setName(String n) {
-        name = n;
+    public Student() {}
+
+    public Student(String name) {
+        this.name = name;
     }
 
-    public String getName() {
-        return name;
+    public Student(int age) {
+        this.age = age;
     }
 
-    public void setAge(int a) {
-        age = a;
-    }
-
-    public int getAge() {
-        return age;
+    public Student(String name,int age) {
+        this.name = name;
+        this.age = age;
     }
 
     public void show() {
